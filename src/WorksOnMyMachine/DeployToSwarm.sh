40350a3839d6b01bb6686d4d8f5c4051dc854bc4
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-05-27 13:41:41 -0700

    update DeployToSwarm.sh

--- src/WorksOnMyMachine/DeployToSwarm.sh
+++ src/WorksOnMyMachine/DeployToSwarm.sh
@@ -3,4 +3,5 @@
 eval `ssh-agent`
 ssh-add ~/.ssh/dazhao-swarm/id_rsa
 scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com:~/docker-compose.sh
-ssh azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com -A -p 2200 'bash -s' < ./RunOnSwarm.sh
+scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/RunOnSwarm.sh azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com:~/RunOnSwarm.sh
+ssh azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com -A -p 2200 'chmod +x ~/RunOnSwarm.sh && bash -s ~/RunOnSwarm.sh'
