bc46f7e88d1d8222691bd5cdd4da0bdf15d2e53d
Weida Zhao <dazhao@microsoft.com>
2016-05-31 13:21:11 -0700

    point to a different cluster

--- src/WorksOnMyMachine/DeployToSwarm.sh
+++ src/WorksOnMyMachine/DeployToSwarm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval `ssh-agent`
-ssh-add ~/.ssh/dazhao-swarm/id_rsa
-scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com:~/docker-compose.yml
-scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/RunOnSwarm.sh azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com:~/RunOnSwarm.sh
-ssh azureuser@dazhao-swarmmgmt.westus.cloudapp.azure.com -A -p 2200 'chmod +x ~/RunOnSwarm.sh && ~/RunOnSwarm.sh'
+ssh-add ~/.ssh/acs-swarm/id_rsa
+scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml azuredevex@azuredevex-acsmgmt.southcentralus.cloudapp.azure.com:~/docker-compose.yml
+scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/RunOnSwarm.sh azuredevex@azuredevex-acsmgmt.southcentralus.cloudapp.azure.com:~/RunOnSwarm.sh
+ssh azuredevex@azuredevex-acsmgmt.southcentralus.cloudapp.azure.com -A -p 2200 'chmod +x ~/RunOnSwarm.sh && ~/RunOnSwarm.sh'
