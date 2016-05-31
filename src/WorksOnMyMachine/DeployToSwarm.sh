963a444edf3a81e2ea7926dbca165b8854b4006c
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-05-31 16:34:01 -0700

    Adds comment

--- src/WorksOnMyMachine/DeployToSwarm.sh
+++ src/WorksOnMyMachine/DeployToSwarm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# The authentication part is just a PoC.
 eval `ssh-agent`
 ssh-add $ACS_SSHKEY
+
 scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml $ACS_MASTER0:~/docker-compose.yml
 scp -P 2200 $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/RunOnSwarm.sh $ACS_MASTER0:~/RunOnSwarm.sh
 ssh $ACS_MASTER0 -A -p 2200 'chmod +x ~/RunOnSwarm.sh && ~/RunOnSwarm.sh'
