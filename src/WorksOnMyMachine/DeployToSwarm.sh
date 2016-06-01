42dc17fc77eb60d48444cfca4fed44aea129a8bc
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-01 00:45:20 -0700

    update scripts

--- src/WorksOnMyMachine/DeployToSwarm.sh
+++ src/WorksOnMyMachine/DeployToSwarm.sh
@@ -8,6 +8,5 @@ pushd bin/$BUILD_CONFIGURATION/netcoreapp1.0/publish/
 docker build -t weidazhao/worksonmymachine:latest .
 docker push weidazhao/worksonmymachine:latest
 
-scp $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml $ACS_AGENT0:~/docker-compose.yml
-scp $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/RunOnSwarm.sh $ACS_AGENT0:~/RunOnSwarm.sh
-ssh $ACS_AGENT0 'chmod +x ~/RunOnSwarm.sh && ~/RunOnSwarm.sh'
+docker-compose -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml down --rmi all
+docker-compose -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml up -d
