015967578ae0954c22971d4bdfa3f60a1f2e5163
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-01 09:06:11 -0700

    update script

--- src/WorksOnMyMachine/BuildAndDeploy.sh
+++ src/WorksOnMyMachine/BuildAndDeploy.sh
@@ -5,8 +5,10 @@ dotnet publish -c $BUILD_CONFIGURATION
 
 pushd bin/$BUILD_CONFIGURATION/netcoreapp1.0/publish/
 
-docker build -t weidazhao/worksonmymachine:latest .
-docker push weidazhao/worksonmymachine:latest
+docker -H=$DOCKER_HOST_LOCAL build -t weidazhao/worksonmymachine:latest .
+docker -H=$DOCKER_HOST_LOCAL push weidazhao/worksonmymachine:latest
 
-docker-compose -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml down --rmi all
-docker-compose -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml up -d
+popd
+
+docker-compose -H=$DOCKER_HOST_AGENT0 -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml down --rmi all
+docker-compose -H=$DOCKER_HOST_AGENT0 -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml up -d
