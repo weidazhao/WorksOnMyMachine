d8e4972404ee898b7709452e482ef9f273aa7128
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-02 15:19:54 -0700

    Revert "update script"
    This reverts commit 03979ceda6cb50786dbe106ed084a3466d0b4519.

--- src/WorksOnMyMachine/BuildAndDeploy.sh
+++ src/WorksOnMyMachine/BuildAndDeploy.sh
@@ -4,12 +4,11 @@ dotnet restore
 dotnet publish -c $BUILD_CONFIGURATION -o $BUILD_STAGINGDIRECTORY/app/
 
 pushd $BUILD_STAGINGDIRECTORY/app/
-docker -H=$DOCKER_HOST_LOCAL build -t weidazhao/worksonmymachine:latest .
-popd
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker -H=$DOCKER_HOST_LOCAL build -t weidazhao/worksonmymachine:latest .
 docker -H=$DOCKER_HOST_LOCAL push weidazhao/worksonmymachine:latest
-docker logout
+
+popd
 
 docker-compose -H=$DOCKER_HOST_AGENT0 -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml down --rmi all --remove-orphans
 docker-compose -H=$DOCKER_HOST_AGENT0 -f $BUILD_SOURCESDIRECTORY/src/WorksOnMyMachine/docker-compose.yml up -d
