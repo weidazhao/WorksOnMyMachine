92cf623c21f8409c7fcce62864e47deed094a35c
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-08 00:58:47 -0700

    test

--- src/WorksOnMyMachine/Controllers/HomeController.cs
+++ src/WorksOnMyMachine/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WorksOnMyMachine.Controllers
         }
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page. 1";
+            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
