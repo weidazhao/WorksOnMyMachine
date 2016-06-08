f8135ed0637381ae8946971b3966cf69087ddceb
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-06-08 01:13:16 -0700

    update

--- src/WorksOnMyMachine/build-deploy.sh
+++ src/WorksOnMyMachine/build-deploy.sh
@@ -4,8 +4,6 @@ BUILD_CONFIGURATION=Release
 PROJECT_NAME=WorksOnMyMachine
 IMAGE_NAME=weidazhao/worksonmymachine
 
-docker rmi -f $IMAGE_NAME:build
-
 docker build -t $IMAGE_NAME:build -f Dockerfile.build --build-arg IMAGE_NAME=$IMAGE_NAME:latest --build-arg PROJECT_NAME=$PROJECT_NAME --build-arg BUILD_CONFIGURATION=$BUILD_CONFIGURATION .
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME:build
